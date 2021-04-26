3c8c6b25504d298255aacbfe7f7e09c127139c73
Akid-Abrar <akidabrar@gmail.com>
2021-04-26 09:25:55 +0600

    Post Updated

--- models/postmodel.js
+++ models/postmodel.js
@@ -2,14 +2,16 @@ const mongoose=require('mongoose');
 
 const postSchema=mongoose.Schema({
     _id:Number,
-    type:Number,
+    type:Number,//type=1 for discussion,2 for review,3 for comment
     like:Number,
-    bookid:Number,
+    bookid:Number,//0 for comment and discussion
     creatorid:Number,
     content:String,
-    date:Date,
-    commentsIds:[Number],
+    commentsIds:[Number],//empty string for comment,because cant comment on a comment
     approved:Boolean
+},
+{
+    timestamps: true,
 })
 
 
