cc796d394867fa2580016ad79a9d5c465f413a7b
murshedatasnim <1605095@ugrad.cse.buet.ac.bd>
2021-06-10 01:07:32 +0600

    new front end

--- back-end/models/reader.model.js
+++ back-end/models/reader.model.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 
 const readerSchema = new Schema({
   //_id: { type:Number },
-  first_name: { type: String, required: true },
-  last_name:{type:String,required:true},
+  first_name: { type: String, required: false },
+  last_name:{type:String,required:false},
   email:{type:String,required:true},
   password:{type:String,required:true},
   is_author:{type:Boolean },
---
+++ front-end/src/components/SignUp/index.js
@@ -0,0 +1,186 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+import * as ROLES from '../../constants/roles';
+import axios from 'axios';
+
+const SignUpPage = () => (
+  <div>
+    <h1>SignUp</h1>
+    <SignUpForm />
+  </div>
+);
+
+const INITIAL_STATE = {
+  firstname: '',
+  lastname: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  isAdmin: false,
+  error: null,
+};
+
+const ERROR_CODE_ACCOUNT_EXISTS = 'auth/email-already-in-use';
+
+const ERROR_MSG_ACCOUNT_EXISTS = `
+  An account with this E-Mail address already exists.
+  Try to login with this account instead. If you think the
+  account is already used from one of the social logins, try
+  to sign in with one of them. Afterward, associate your accounts
+  on your personal account page.
+`;
+
+class SignUpFormBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { firstname, lastname, email, passwordOne, isAdmin } = this.state;
+    const roles = {};
+
+    if (isAdmin) {
+      roles[ROLES.ADMIN] = ROLES.ADMIN;
+    }
+
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        // Create a user in your Firebase realtime database
+        axios.post('http://localhost:3000/readers/add',{
+          first_name:firstname,
+          last_name:lastname,
+          email:this.state.email,
+          password:this.state.passwordOne,
+          
+        }).then(response =>
+        console.log(response.data)
+      )
+      return this.props.firebase.user(authUser.user.uid).set({
+        firstname,
+        lastname,
+        email,
+        roles,
+      });
+
+      })
+      .then(() => {
+        return this.props.firebase.doSendEmailVerification();
+      })
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+          error.message = ERROR_MSG_ACCOUNT_EXISTS;
+        }
+
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  onChangeCheckbox = event => {
+    this.setState({ [event.target.name]: event.target.checked });
+  };
+
+  render() {
+    const {
+      firstname,
+      lastname,
+      email,
+      passwordOne,
+      passwordTwo,
+      isAdmin,
+      error,
+    } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      firstname === '' ||
+      lastname === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="firstname"
+          value={firstname}
+          onChange={this.onChange}
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          name="lastname"
+          value={lastname}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Last Name"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <label>
+          Admin:
+          <input
+            name="isAdmin"
+            type="checkbox"
+            checked={isAdmin}
+            onChange={this.onChangeCheckbox}
+          />
+        </label>
+        <button disabled={isInvalid} type="submit">
+          Sign Up
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+const SignUpForm = compose(
+  withRouter,
+  withFirebase,
+)(SignUpFormBase);
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
