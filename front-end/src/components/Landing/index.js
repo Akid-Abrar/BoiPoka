6b89153170eb024a151389c3767384886b9a326f
Akid-Abrar <akidabrar@gmail.com>
2021-06-11 00:39:33 +0600

    Working-5

--- front-end/src/components/Landing/index.js
+++ front-end/src/components/Landing/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import {Component} from 'react'
 
-const Landing = () => (
-  
-  <div>
-    <div>
-    
-       <h1>Landing</h1>
-       <h1>Welcome </h1>
-       <h1>Welcome To BoiPoka</h1>
-       <h1>Vallage Na Mara Jabo</h1>
-       <h1>Etto Pera</h1>
-    </div>
-    
-  </div>
-);
+class Landing extends Component
+{
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+          items: [],
+        }
+    }
+
+    componentDidMount()
+    {
+        fetch('http://localhost:3000/readers/60c2081de75d4811348ac3ee')
+          .then(res => res.json())
+          .then(json =>{
+              this.setState({
+                items: json,
+              })
+        });
+    }
+
+    render(){
+        var {items}= this.state;
+        var f_name=items.first_name;
+        var l_name=items.last_name;
+        var name=f_name+" "+l_name;
+        return(
+          <div>
+              <h1>{name}</h1>
+          </div>
+
+        );
+    }
+}
+
 
 export default Landing;
